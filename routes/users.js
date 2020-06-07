bb1dc8e552701c6cff901ae0ae22d7d756a84a2a
sony <sony@VAIO>
2020-06-07 13:10:04 -0700

    Layout Update

--- routes/users.js
+++ routes/users.js
@@ -3,24 +3,21 @@ var router = express.Router();
 const { check, validationResult } = require('express-validator');
 var loginController = require(process.cwd()+'/controllers/loginController');
 /**************Login Post and get menthod***************/
-
 router.get('/', function(request, response, next) { 
 		response.render('index', {layout: false,title: 'Admin Login',error_message:'',error:''});
 });
-
 router.get('/login', function(request, response, next) { 
 		response.render('index', {layout: false,title: 'Admin Login',error_message:'',error:''});
 });
 router.post('/login',loginController.validate('login'),loginController.login);
-
-
 router.get('/gernatepassword',loginController.gernatepassword);
 
 
+
+/**************Dashboard Route***************/
 router.get('/dashboard', function(request, response, next) { 
 		response.render('dashboard/index', {title:'E-Learning',layout:true});
 });
-
 
 
 module.exports = router;
